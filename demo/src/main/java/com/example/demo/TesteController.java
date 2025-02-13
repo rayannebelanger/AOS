b3498fa2d953cef20021277eceab86dc5251e790
Rayanne Belanger <135059362+rayannebelanger@users.noreply.github.com>
2025-02-13 22:34:00 +0000

    first commit

---
+++ demo/src/main/java/com/example/demo/TesteController.java
@@ -0,0 +1,12 @@
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello ";
+    }
+}
